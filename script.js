f0b3a1305ea93414abbdb24926834137263be561
serendatapy <arvaldivia@gmail.com>
2020-06-07 20:04:19 +0200

    added party color property

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-var makePolitician = function (polName,results,totalVotes) {
+var makePolitician = function (polName,results,totalVotes,partyColor) {
   var politician = {};
     politician.name = polName;
+    politician.pColor = partyColor;
     politician.electionResults = results;
     politician.totalVotes = totalVotes;
     politician.correctVote = function(index,newValue){
@@ -15,13 +16,13 @@ var makePolitician = function (polName,results,totalVotes) {
   return politician;
 }
 
-var harveyDent = makePolitician("Harvey Dent",null,0);
-var obama = makePolitician("Obama",null,0);
+var harveyDent = makePolitician("Harvey Dent",null,0,[132, 17, 11]);
+var obama = makePolitician("Obama",null,0,[245, 141, 136]);
 
 harveyDent.electionResults = [5,1,7,2,33,6,4,2,1,14,8,3,1,11,11,0,5,3,3,3,7,4,8,9,3,7,2,2,4,2,8,3,15,15,2,12,0,4,13,1,3,2,8,21,3,2,11,1,3,7,2];
 obama.electionResults = [4,2,4,4,22,3,3,1,2,15,8,1,3,9,0,6,1,5,5,1,3,7,8,1,3,3,1,3,2,2,6,2,14,0,1,6,7,3,7,3,6,1,3,17,3,1,2,11,2,3,1];
 
-//console.log("Politician1: ",harveyDent,"Politician 2: ",obama);
+console.log("Politician1: ",harveyDent,"Politician 2: ",obama);
 
 
 harveyDent.correctVote(9,1);
@@ -36,4 +37,4 @@ obama.countV();
 console.log(harveyDent.totalVotes,obama.totalVotes);
 
 var winner = harveyDent.totalVotes > obama.totalVotes ? harveryDent.name : obama.name;
-console.log(winner);
+//console.log(winner);
